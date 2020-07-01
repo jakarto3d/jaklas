77642783ae0178156bc67cc0d86dee4136f4f46d
David Caron <dcaron05@gmail.com>
2020-07-01 11:32:41 -0400

    implement point_format handling

---
+++ src/las_writer/point_formats.py
@@ -0,0 +1,40 @@
+# Note some less often used attributes are omitted
+
+_base = {
+    "x": "i4",
+    "y": "i4",
+    "z": "i4",
+    "intensity": "u2",
+    "classification": "u1",
+}
+_gps_time = {"gps_time": "f8"}
+_colors = {
+    "red": "u2",
+    "green": "u2",
+    "blue": "u2",
+}
+point_formats = {
+    0: {**_base},
+    1: {**_base, **_gps_time},
+    2: {**_base, **_colors},
+    3: {**_base, **_gps_time, **_colors},
+}
+
+
+def guess_best_format(data) -> int:
+    """Returns the best point format depending on keys in the provided object.
+
+    Args:
+        data (dict-like): Any object that implements the __getitem__ method.
+            So a dictionnary, a pandas DataFrame, or a numpy structured array will
+            all work.
+
+    Returns:
+        int: The best point format. If none is matched, return 0 as a default.
+    """
+    possible_formats = [
+        n for n, f in point_formats.items() if all(k in data for k in f)
+    ]
+    if not possible_formats:
+        return 0
+    return min(possible_formats, key=lambda n: len(point_formats[n]))
--- src/las_writer/write.py
+++ src/las_writer/write.py
@@ -1,12 +1,17 @@
 from pathlib import Path
-from typing import Union
+from typing import Union, Tuple
 
 import numpy as np
 import laspy
 
+from . import point_formats
+
 
 def write(
-    point_data, output_path: Union[Path, str], precision=(0.0001, 0.0001, 0.0001)
+    point_data,
+    output_path: Union[Path, str],
+    point_format: int = None,
+    precision: Tuple[float] = (0.0001, 0.0001, 0.0001),
 ):
     """Write point cloud data to an output path.
 
@@ -15,8 +20,11 @@ def write(
             So a dictionnary, a pandas DataFrame, or a numpy structured array will
             all work.
         output_path (Union[Path, str]): The output path to write the las file.
-            If the output directory doesn't exist, it's created.
-        precision (tuple, optional): The coordinate precision.
+            The output directory is created if it doesn't exist.
+        point_format (int, optional): The las point format type (only formats 0 to 3
+            are accepted). If None is given, the best point format will be guessed
+            based on the provided fields.
+        precision (Tuple[float], optional): The coordinate precision.
             Coordinates in the las file are stored in int32. This means that there is
             always a slight loss in precision. Most real world use cases are not
             affected if the scale is set correctly. So this should be set to the
@@ -26,8 +34,15 @@ def write(
             Defaults to (0.0001, 0.0001, 0.0001).
     """
     Path(output_path).parent.mkdir(parents=True, exist_ok=True)
+    
+    if not point_format:
+        point_format = point_formats.guess_best_format(point_data)
 
-    with laspy.file.File(str(output_path), mode="w", header=laspy.header.Header()) as f:
+    with laspy.file.File(
+        str(output_path),
+        mode="w",
+        header=laspy.header.Header(point_format=point_format),
+    ) as f:
         f.header.offset = [
             np.min(point_data["x"]),
             np.min(point_data["y"]),
