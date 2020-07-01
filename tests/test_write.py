9baec4bcc397dabe194b95ee01292f824e1dc4dd
David Caron <dcaron05@gmail.com>
2020-07-01 10:59:30 -0400

    fix test

--- tests/test_write.py
+++ tests/test_write.py
@@ -11,23 +11,15 @@ import las_writer
 DATA_DIR = Path(__file__).parent / "data"
 
 
-@pytest.fixture
-def xyz():
-    return np.random.random((100, 3)) * 100
+xyz = np.random.random((100, 3)) * 100
 
+point_data_dict = {
+    "x": xyz[:, 0],
+    "y": xyz[:, 1],
+    "z": xyz[:, 2],
+}
 
-@pytest.fixture
-def point_data_dict(xyz):
-    return {
-        "x": xyz[:, 0],
-        "y": xyz[:, 1],
-        "z": xyz[:, 2],
-    }
-
-
-@pytest.fixture
-def point_data_pandas(point_data_dict):
-    return pd.DataFrame(point_data_dict)
+point_data_pandas = pd.DataFrame(point_data_dict)
 
 
 @pytest.mark.parametrize("point_data", [point_data_dict, point_data_pandas])
@@ -35,6 +27,6 @@ def test_write_dict(point_data):
     output = DATA_DIR / "temp.las"
     las_writer.write(point_data, output)
     with laspy.file.File(output) as f:
-        assert np.all_close(f.x == point_data["x"], e=0.0001)
-        assert np.all_close(f.y == point_data["y"], e=0.0001)
-        assert np.all_close(f.z == point_data["z"], e=0.0001)
+        assert np.allclose(f.x, point_data["x"], atol=0.0001)
+        assert np.allclose(f.y, point_data["y"], atol=0.0001)
+        assert np.allclose(f.z, point_data["z"], atol=0.0001)
